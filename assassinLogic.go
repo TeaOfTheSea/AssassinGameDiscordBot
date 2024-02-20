e7c4b4051ce2a62a472e058edf31c41716cd7ced
TeaOfTheSea <coltipto@ttu.edu>
2024-02-20 12:59:45 -0600

    StringToLL passing for single user strings

--- assassinLogic.go
+++ assassinLogic.go
@@ -3,8 +3,18 @@ package assassinlogic
 import (
 	"container/list"
 	"errors"
+	"strings"
 )
 
 func StringToLL(s string) (*list.List, error) {
-	return list.New(), errors.New("No input given.")
+	// Return empty list and error if input string is empty
+	if s == "" {
+		return list.New(), errors.New("No input given.")
+	}
+	var sValues []string = strings.Split(s, " -> ")
+	output := list.New()
+	for _, v := range sValues {
+		output.PushFront(v)
+	}
+	return output, nil
 }
--- assassinLogic_test.go
+++ assassinLogic_test.go
@@ -1,17 +1,38 @@
 package assassinlogic
 
 import (
+	"container/list"
+	"errors"
+	"fmt"
 	"testing"
-  "errors"
-  "fmt"
 )
 
 func TestStringToLL(t *testing.T) {
-  t.Run("Passing an empty string and checking for error", func(t* testing.T){
-	  _, got := StringToLL("")
-    want := errors.New("No input given.")
-    if fmt.Sprint(got) != fmt.Sprint(want) {
-      t.Errorf("got %q want %q", got, want)
-    }
-  })
+	t.Run("Passing an empty string and checking for error", func(t *testing.T) {
+		_, got := StringToLL("")
+		want := errors.New("No input given.")
+		if fmt.Sprint(got) != fmt.Sprint(want) {
+			t.Errorf("got %q want %q", got, want)
+		}
+	})
+	t.Run("Passing a single user", func(t *testing.T) {
+		// Getting output and checking for errors before output
+		got, err := StringToLL("Colin")
+		if err != nil {
+			t.Errorf(fmt.Sprint(err))
+		}
+		want := list.New()
+		want.PushFront("Colin")
+		eGot := got.Front()
+		for eWant := want.Front(); eWant != nil; eWant = eWant.Next() {
+			if eGot == nil {
+				t.Errorf("list got storter than list want")
+				break
+			}
+			if eGot.Value != eWant.Value {
+				t.Errorf("In linked list, got %q want %q", eGot.Value, eWant.Value)
+			}
+			eGot = eGot.Next()
+		}
+	})
 }
